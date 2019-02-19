2e28788116cfe5d6e8c1ae5fa3ff1b36aa6efe7c
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-02-19 19:45:19 +0100

    Add: Docs for tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,9 @@
+"""Test the db functionality for the EClass."""
 import sqlite3
 
 import pytest
 
-from e_class.db import get_db, close_db
+from e_class.db import get_db
 
 
 def test_get_close_db(app):
@@ -10,6 +11,7 @@ def test_get_close_db(app):
         db = get_db()
         assert db is get_db()
 
+    # after the context the db connection gets teared down
     with pytest.raises(sqlite3.ProgrammingError) as e:
         db.execute('SELECT 1')
 
@@ -17,13 +19,12 @@ def test_get_close_db(app):
 
 
 def test_init_db_command(runner, monkeypatch):
-    class Recorder(object):
-        called = False
+    init = {'called': False}
 
     def fake_init_db():
-        Recorder.called = True
+        init['called'] = True
 
     monkeypatch.setattr('e_class.db.init_db', fake_init_db)
     result = runner.invoke(args=['init-db'])
     assert 'Initialized' in result.output
-    assert Recorder.called
+    assert init['called']
