0c5203eb5ea6025bf817a946f729bbfc57de8b6b
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-02-19 17:10:06 +0100

    Add: Factory and db access from tutorial

---
+++ e_class/__init__.py
@@ -0,0 +1,2 @@
+from e_class.main import create_app
+from e_class.db import get_db, close_db, init_db
---
+++ e_class/db.py
@@ -0,0 +1,41 @@
+"""Functions to access the web-database."""
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            database=current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
+def close_db(e=None):
+    # e is needed for the teardown and is needed for an error object
+    db = g.pop(name='db', default=None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
