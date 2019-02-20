828e2152337a0d27b8ebd35bf952a661fb1b8249
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-02-20 11:31:58 +0100

    Add: Tests for registering a new user

---
+++ tests/test_auth.py
@@ -0,0 +1,32 @@
+import pytest
+
+from e_class.db import get_db
+
+
+def test_register(client, app):
+    # register is available
+    assert client.get('/auth/register').status_code == 200
+    response = client.post(
+        '/auth/register',
+        data={'email': 'a', 'password': 'a'}
+    )
+    # after successful register reroute to login
+    assert 'http://localhost/auth/login' == response.headers['Location']
+
+    with app.app_context():
+        # test user a is in database
+        assert get_db().execute(
+            'SELECT * FROM user WHERE email = "a"', ).fetchone() is not None
+
+
+@pytest.mark.parametrize(('email', 'password', 'message'), (
+    ('', '', b'Email adress is required.'),
+    ('a', '', b'Password is required.'),
+    ('test1', 'test', b'already registered.')
+))
+def test_register_validate_input(client, email, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'email': email, 'password': password}
+    )
+    assert message in response.data
