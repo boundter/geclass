8737575c1551bc071d4da5b9c42244641d20f3c7
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-02-20 10:07:00 +0100

    Add: Function for logout

--- e_class/auth.py
+++ e_class/auth.py
@@ -56,7 +56,12 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('auth.register'))
         flash(error)
     return render_template('auth/login.html')
 
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.register'))
