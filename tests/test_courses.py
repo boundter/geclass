233c75defe310a2cebc039af695873ac28bd7ada
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-02-21 15:38:04 +0100

    Add: course identifier to overview

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -10,3 +10,8 @@ def test_only_registered(client, auth):
     print(response.data)
     print(response.headers)
     assert b'Your courses' in response.data
+
+    # only own courses appear
+    assert b'uni_potsdam_biochem_2018' in response.data
+    assert b'uni_potsdam_phys_2018' in response.data
+    assert b'uni_hamburg_phys_2018' not in response.data
