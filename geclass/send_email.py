82babca71a5ff608653b95ea3d33cec6bb81ce79
Erik Teichmann <eteichma@uni-potsdam.de>
2020-02-19 15:24:04 +0100

    Fix: Send Email with proper body

---
+++ geclass/send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+
+def SendMail(recipient, subject, content):
+    server_ip = "172.17.0.1"  # ip of the smtp server
+    sender_mail = "no-reply@geclass.physik.uni-potsdam.de"
+    content += "\n\n-----------------------\n"
+    content += "Bitte antworten Sie nicht auf diese Email, nutzen Sie stattdessen ge-class@uni-potsdam.de"
+    message = EmailMessage()
+    message.set_content(content)
+    message["Subject"] = subject
+    message["From"] = "GEClass <no-reply@geclass.physik.uni-potsdam.de>"
+    message["To"] = recipient
+    with smtplib.SMTP(server_ip) as server:
+        server.send_message(message)
